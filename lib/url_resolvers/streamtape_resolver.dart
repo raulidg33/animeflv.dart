49e8d118467fd60b0f74ec5680767178b18a5bb8
Raúl Dastres Gallardo <rdastres@icloud.com>
2022-02-06 21:59:01 -0300

    added streamtape url resolver

--- lib/url_resolvers/streamtape_resolver.dart
+++ lib/url_resolvers/streamtape_resolver.dart
@@ -15,14 +15,10 @@ class StreamTapeResolver {
       String srcUrl = '';
       final parts = src[src.groupCount]!.replaceAll("'", '"').split('+');
       for (var part in parts) {
-        String p1 = RegExp(r'"([^"]*)', caseSensitive: false)
-            .firstMatch(part)!
-            .group(1)
-            .toString();
+        String p1 = RegExp(r'"([^"]*)', caseSensitive: false).firstMatch(part)!.group(1).toString();
         int p2 = 0;
         if (part.contains('substring')) {
-          final subs = RegExp(r'substring\((\d+)\)', caseSensitive: false)
-              .allMatches(part);
+          final subs = RegExp(r'substring\((\d+)\)', caseSensitive: false).allMatches(part);
           for (var sub in subs) {
             p2 += int.parse(sub.group(1).toString());
           }
@@ -31,7 +27,13 @@ class StreamTapeResolver {
       }
       srcUrl += '&stream=1';
       srcUrl = srcUrl.startsWith('//') ? 'https:$srcUrl' : srcUrl;
-      return [srcUrl];
+      // return [srcUrl];
+      return [
+        {
+          'label': '720p',
+          'file': srcUrl,
+        }
+      ];
     } catch (_) {}
     return [];
   }
